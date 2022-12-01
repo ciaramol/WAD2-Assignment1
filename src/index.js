a67cc802e77894fbd34b73f5c9cb9992c72215e1
Ciara Molloy <20089106@mail.wit.ie>
2022-12-01 17:37:57 +0000

    Lab Movies App + Popular movies page.

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Navigate, Routes } from "react-router-dom";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
+import FavouriteMoviesPage from "./pages/favouriteMoviesPage";
+import PopularMoviesPage from "./pages/popularMoviesPage";
+import { Link } from 'react-router-dom';
+import MovieReviewPage from "./pages/movieReviewPage";
+import SiteHeader from './components/siteHeader';
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from 'react-query/devtools';
+import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 360000,
+      refetchInterval: 360000,
+      refetchOnWindowFocus: false
+    },
+  },
+});
+
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <SiteHeader /> {/*New Header */}
+        <MoviesContextProvider>
+          <Routes>
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
+            <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route exact path="/movies/upcoming" element={<UpcomingMoviesPage />} />
+            <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
+            <Route exact path="/movies/popular" element={<PopularMoviesPage /> } />
+            <Route path="/movies/:id" element={<MoviePage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </MoviesContextProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+
+const rootElement = createRoot(document.getElementById("root"))
+rootElement.render(<App />);
